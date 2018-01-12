ed511e37ce7d0d52110a714cbe6f69d786b55126
LyngeLauritsen <brp701@alumni.ku.dk>
2018-01-12 12:10:00 +0100

    FRI-Kelly2009

---
+++ MasterP/Kelly2009.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a piece of code to recreate the Kelly et al. 2009 paper.
+"""
+
+import numpy as np
+import scipy as sc
+import matplotlib.pyplot as plt
+import random
+import math
+from scipy.optimize import minimize as mini
+from scipy.optimize import least_squares as ls
+from scipy.optimize import leastsq
+import pyOpt
+from pyOpt import CONMIN
+from pyOpt import Optimization
+
+'''x denotes the observed fluxes, and t the observation times, 
+sigma is the error variances and p is the probability function'''
+
+global_variable_share = np.loadtxt('timefluxsigma.txt')
+global_variable_share_2 = np.loadtxt('time1flux1sigma1.txt')
+
+time = global_variable_share[0]
+flux = global_variable_share[1]
+sigma = global_variable_share[2]
+
+time1 = global_variable_share_2[0]
+flux1 = global_variable_share_2[1]
+sigma1 = global_variable_share_2[2]
+
+def a_i(t2,t1,tau):
+    tau = float(tau)
+    return np.exp(-(t2-t1)/tau)
+
+def omega_0(tau,sigma_tot):
+    return 0.5*tau*sigma_tot**2
+
+def omega_i(omega_0,a_i,omega_before,sigma_before):
+    return omega_0*(1-a_i**2) + a_i**2*omega_before*(1 - omega_before/(omega_before + sigma_before**2))
+
+def x_hat_i(a_i,x_hat_before,omega_before,sigma_before,x_star_before):
+    return a_i*x_hat_before + ((a_i*omega_before)/((omega_before + sigma_before**2)))*(x_star_before - x_hat_before)
+
+def x_star_i(x_i,b,tau):
+    return x_i - b*tau
+
+def parameters(param):
+    x_hat = [0]
+    omega = [0.5*param[1]*param[2]**2]
+    x_star = [-param[0]*param[1]]
+    a = []
+    for i in range(len(flux1)):
+        a.append(a_i(time[i+1],time[i],param[1]))
+        x_hat.append(x_hat_i(a[i],x_hat[i],omega[i],sigma[i],x_star[i]))
+        omega.append(omega_i(omega_0(param[1],param[2]),a[i],omega[i],sigma[i]))
+        x_star.append(x_star_i(flux[i+1],param[0],param[1]))
+    x_hat[0] = np.mean(x_hat)
+    x_hat[1] = np.mean(x_hat)
+    return x_hat,omega,x_star,a
+
+def prob(param):
+    x_hat,omega,x_star,a = parameters(param)
+    #print x_hat
+    #print x_star
+    #print omega
+    
+    probability = 1
+    for i in range(len(a)):
+        #print omega[i+1],sigma[i+1]**2 #,x_hat[i+1],x_star[i+1],a[i]
+        #print (x_hat[i+1] - x_star[i+1])
+        part = np.log(1/np.sqrt(2*np.pi*(omega[i+1] + sigma[i+1]**2)))\
+        -(1/2.)*(((x_hat[i+1] - x_star[i+1])**2.)/(omega[i+1] + sigma[i+1]**2.))
+        #print part,sigma1[i]
+        #print ((x_hat[i+1] - x_star[i+1])**2.)/(omega[i+1] + sigma[i+1]**2.)
+        #print 1/np.sqrt(2*np.pi*(omega[i+1] + sigma[i+1]**2))
+        probability += part
+        #print part, probability
+    return -probability
+
+def dX(tau,sigma,dt,b,epsilon,X):
+    dt = dt
+    sigma = sigma
+    return (-(1/tau)*X*dt + sigma*np.sqrt(dt)*epsilon + b*dt)
+
+def flux_model(param):
+    res = mini(prob,param,method='Nelder-Mead',tol=1e-18)
+    tau = res.x[1] #1400. #res.x[1] #1400. #res.x[1]
+    b = res.x[0] #9.2/tau #res.x[0] #7.5*10**(-15)/tau #res.x[0]
+    sigma_tot = res.x[2] #0.01 #res.x[2] #1.3*10**(-16) #res.x[2] 
+    model = np.zeros((2000,len(flux1)))
+    flux_test = flux1[0]
+    for j in range(len(model[:,0])):
+        model[j,0] = flux1[0]
+        #flux_model = [flux1[0]]
+        #print j
+        for i in range(len(flux1)-1):
+            #print i, model[j,i]
+            dt = (time1[i+1]-time1[i])
+            epsilon = np.random.normal(0,1,1)
+            dX1 = (flux1[i+1])#*(-1) + model[j,0]
+            change = dX(tau,sigma_tot,dt,b,epsilon,dX1) #flux1[i+1] - flux1[i]
+            #print change,flux1[i+1]
+            model[j,i+1] = change + flux1[i] #model[j,i] # - flux_test)
+    model2 = []
+    time_model = []
+    std = []
+    for i in range(len(model[0,:])):
+        model2.append(np.mean(model[:,i]))
+        std.append(np.std(model[:,i]))
+        time_model.append(time1[i])
+    flux_model = np.array((time_model,model2,std,res.x))
+    return flux_model
